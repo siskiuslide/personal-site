05aa801b6c34130e0997abd1f8622a4fbeb1961e
siskiuslide <jakewhatling2002@gmail.com>
2022-11-21 14:12:47 +0000

    homepage styling

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -5,8 +5,20 @@ const Home = function () {
   return (
     <>
       <div className="welcome">Welcome!</div>
-      <div className="name">
-        I'm Jake, a <span className="nounSpan"> motor enthusiast </span>
+      <div className="about">
+        <p className="name">
+          I'm Jake, a <span className="nounSpan"> motor enthusiast </span>
+        </p>
+        <p className="location">
+          <span className="material-icons" style={{ fontSize: "0.75em" }}>
+            location_on
+          </span>
+          Based in Warrington
+        </p>
+      </div>
+      <div className="introduction">
+        <p className="explainTag">Allow me to explain</p>
+        <p className="text">Firstly, I'm a Web Developer.</p>
       </div>
     </>
   );
