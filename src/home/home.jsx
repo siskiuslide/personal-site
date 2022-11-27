89608ac80a0e7d968af07d46c48cc795150f61cb
siskiuslide <jakewhatling2002@gmail.com>
2022-11-27 19:43:59 +0000

    added browse section to homepage

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./home.css";
 import Card from "../components/Card";
 
 const Home = function () {
-  const { noun, setNoun } = useState(["Web Developer", "Photographer", "Motor Enthusiast"]);
+  const [noun, setNoun] = useState(["Web Developer", "Photographer", "Motor Enthusiast"]);
+  const [arrowIcon, setArrowIcon] = useState("keyboard_arrow_down");
+  const [showCards, setShowCards] = useState(false);
 
+  const arrowHandler = function () {
+    if (arrowIcon === "keyboard_arrow_down") {
+      setArrowIcon("keyboard_arrow_up");
+      setShowCards(false);
+      return;
+    } else {
+      setArrowIcon("keyboard_arrow_down");
+      setShowCards(true);
+    }
+  };
   return (
     <>
       <div className="welcome">Welcome!</div>
@@ -19,11 +32,18 @@ const Home = function () {
           Based in Warrington
         </p>
       </div>
-      <div className="cardFlex">
-        <Card title="Coding" desc="View my projects" background="shadowblue"></Card>
-        <Card title="Photography" desc="Browse galleries" background="middlegreen"></Card>
-        <Card title="Motoring" desc="Look at my motoring updates" background="glaucousblue"></Card>
+      <div className="browse" onClick={arrowHandler}>
+        Browse my work <span className="material-icons">{arrowIcon}</span>
       </div>
+      {showCards == true ? (
+        <div className="cardFlex">
+          <Card title="Coding" desc="View my projects" dest="/coding" background="shadowblue"></Card>
+          <Card title="Photography" desc="Browse galleries" dest="/Photography" background="middlegreen"></Card>
+          <Card title="Motoring" desc="Look at my motoring updates" dest="/Motoring" background="glaucousblue"></Card>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 };
