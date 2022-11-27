e059e29a8c896102a3eed125343928abffee105f
siskiuslide <jakewhatling2002@gmail.com>
2022-11-27 19:44:53 +0000

    altered card for homepage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Card.css";
+import { Link } from "react-router-dom";
 const Card = function (props) {
   return (
-    <div className="card" style={{ background: `var(--${props.background})` }}>
+    <Link to={props.dest} className="card" style={{ background: `var(--${props.background})` }}>
       <p className="title">{props.title}</p>
       <p className="desc">{props.desc}</p>
-    </div>
+    </Link>
   );
 };
 
