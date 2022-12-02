c2fc0543805cb42ca16f3559f9b2d765715d6b59
siskiuslide <jakewhatling2002@gmail.com>
2022-12-02 14:25:17 +0000

    coding page progress

--- src/coding/coding.jsx
+++ src/coding/coding.jsx
@@ -16,12 +16,13 @@ const Coding = function () {
           bg="paleblue"
           title="Project Car App"
           desc="An application designed for motoring enthusiasts to organize the ownership of their vehicles, track expenses and create to-do lists."
-          textWi="60%"
+          textWi="75%"
         ></Project>
         <Project
           bg="paleorange"
           title="Control"
           desc="A project that uses the MakerAPI to integrate with users' devices to allow remote control over smart-home devices"
+          textWi="60%"
         ></Project>
         <Project bg="palepurple" title="Smart Home Dashboards" textWi="75%"></Project>
         <Project></Project>
--- src/coding/project.jsx
+++ src/coding/project.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
-import controlButton from "./extras/controlButton";
+import ControlButton from "./extras/controlButton";
 import "./project.css";
+import "./extras/controlButton.css";
 
 const Project = function (props) {
   const [styles, setStyles] = useState({ background: `var(--${props.bg})` });
 
   return (
-    <div className="project" style={styles}>
+    <div className={props.title === "Control" ? "controlProj project" : "project"} style={styles}>
       <p className="title">{props.title}</p>
-      <p className="description" style={{ background: `var(--${props.bg}cont)`, width: props.textWi }}>
-        {props.desc}
-      </p>
-      {props.title === "Control" ? <controlButton /> : ""}
+      {props.title === "Control" ? (
+        <div className="controlDescSection">
+          <p className="description" style={{ background: `var(--${props.bg}cont)`, width: props.textWi }}>
+            {props.desc}
+          </p>
+          <ControlButton className="control control-1" />
+        </div>
+      ) : (
+        <p className="description" style={{ background: `var(--${props.bg}cont)`, width: props.textWi }}>
+          {props.desc}
+        </p>
+      )}
       <div className="screenshot"></div>
     </div>
   );
