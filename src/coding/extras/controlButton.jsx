7c128f0a00d2789374ea76483b69451eb61e8fa4
siskiuslide <jakewhatling2002@gmail.com>
2022-12-02 15:01:47 +0000

    responsiveness styling

--- src/coding/extras/controlButton.jsx
+++ src/coding/extras/controlButton.jsx
@@ -2,17 +2,29 @@ import React, { useState } from "react";
 import "./controlButton.css";
 
 const ControlButton = function () {
-  const [bulbo, setBulbo] = useState({ status: "off", class: "material-symbols-outlined", color: "black" });
+  const [bulbo, setBulbo] = useState({
+    status: "off",
+    class: "material-icons",
+    style: {
+      color: "white",
+      textShadow: "2px 2px 0px black, -2px -2px 0px black, -2px 2px 0px black, 2px -2px 0px black",
+    },
+  });
 
   const bulboHandler = function () {
-    console.log("x");
     if (bulbo.status === "on") {
-      setBulbo({ status: "off", class: "material-symbols-outlined", style: { color: "black" } });
+      setBulbo({
+        status: "off",
+        class: "material-icons",
+        style: {
+          color: "white",
+          textShadow: "2px 2px 0px black, -2px -2px 0px black, -2px 2px 0px black, 2px -2px 0px black",
+        },
+      });
     }
     if (bulbo.status === "off") {
       setBulbo({
         status: "on",
-        class: "material-icons",
         style: {
           color: "yellow",
           textShadow: "2px 2px 0px black, -2px -2px 0px black, -2px 2px 0px black, 2px -2px 0px black",
@@ -24,7 +36,7 @@ const ControlButton = function () {
   return (
     <div className="controlButton">
       <h1>Desk Lamp</h1>
-      <div className={"statusIcon " + bulbo.class} style={bulbo.style} onClick={bulboHandler}>
+      <div className={"statusIcon material-icons"} style={bulbo.style} onClick={bulboHandler}>
         lightbulb
       </div>
       <div className="footer"></div>
