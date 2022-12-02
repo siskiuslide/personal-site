5debe36d5f842180b6b6e3614f7e0f4d3bf0399d
siskiuslide <jakewhatling2002@gmail.com>
2022-12-02 14:24:53 +0000

    project 2 button styling

--- src/coding/extras/controlButton.jsx
+++ src/coding/extras/controlButton.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./controlButton.css";
 
-const controlButton = function () {
+const ControlButton = function () {
+  const [bulbo, setBulbo] = useState({ status: "off", class: "material-symbols-outlined", color: "black" });
+
+  const bulboHandler = function () {
+    console.log("x");
+    if (bulbo.status === "on") {
+      setBulbo({ status: "off", class: "material-symbols-outlined", style: { color: "black" } });
+    }
+    if (bulbo.status === "off") {
+      setBulbo({
+        status: "on",
+        class: "material-icons",
+        style: {
+          color: "yellow",
+          textShadow: "2px 2px 0px black, -2px -2px 0px black, -2px 2px 0px black, 2px -2px 0px black",
+        },
+      });
+    }
+  };
+
   return (
     <div className="controlButton">
       <h1>Desk Lamp</h1>
-      <div className="statusIcon material-icons">lightbulb</div>
+      <div className={"statusIcon " + bulbo.class} style={bulbo.style} onClick={bulboHandler}>
+        lightbulb
+      </div>
       <div className="footer"></div>
     </div>
   );
 };
 
-export default controlButton;
+export default ControlButton;
