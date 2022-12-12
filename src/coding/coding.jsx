47cc9af2d86ef44db0179fc634b866e8ac131c3b
siskiuslide <jakewhatling2002@gmail.com>
2022-12-12 15:11:51 +0000

    coding page progress

--- src/coding/coding.jsx
+++ src/coding/coding.jsx
@@ -16,6 +16,11 @@ const Coding = function () {
           Below is a collection of the programming projects I've led or contributed to so far. My expertise is
           JavaScript development with my skills ranging across the full MERN stack.
         </p>
+
+        <p>
+          My current focus is improving on my React knowledge. I'm learning how to write optimized code using advanced
+          techniques such as custom hooks, the Context API and managing complex state reducers{" "}
+        </p>
         <p>
           Once I have progressed with my React learning, I plan to start learning TypeScript as the language gaining
           huge momentum within the software industry{" "}
@@ -23,21 +28,21 @@ const Coding = function () {
       </div>
       <div className="projectList">
         <Project
-          bg="paleblue"
+          // bg="paleblue"
           title="Project Car App"
           desc="An application designed for motoring enthusiasts to organize the ownership of their vehicles, track expenses and create to-do lists."
           techList={["React", "MongoDB", "Express", "NodeJS"]}
           fadeInDelay="400"
         ></Project>
         <Project
-          bg="paleorange"
+          // bg="paleorange"
           title="Control"
           desc="A project that uses the MakerAPI to integrate with users' devices to allow remote control over smart-home devices"
           techList={["HTML", "CSS", "JS", "MongoDB", "Express", "NodeJS"]}
           fadeInDelay="700"
         ></Project>
         <Project
-          bg="palepurple"
+          // bg="palepurple"
           title="Smart Home Dashboards"
           desc="A collection of CSS templates designed to create an immersive smart-home experience when coupled with interactive dashboards. Designed in relation to the floorplan of my home."
           techList={["CSS"]}
